af860dd50cfa899ec99a865a34e5403d2782bb47
Mamednoor <mamed.gohabur@gmail.com>
2021-05-06 11:47:57 +0200

    update

--- sagas.js
+++ sagas.js
@@ -1,29 +1,26 @@
-import { put, takeEvery, all, call } from 'redux-saga/effects'
+import { put, takeEvery, all, call } from "redux-saga/effects";
 
-export const delay = (ms) => new Promise(res => setTimeout(res, ms))
+export const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 function* helloSaga() {
-  console.log('hello Sagas!     ')
+  console.log("hello Sagas!");
 }
 
 // creation du saga qui va mettre en fonction la tache asynchrone
 export function* incrementAsync() {
-  // utilisation de l'appel 
-  yield call(delay, 1000)
-  yield put({ type: 'INCREMENT' })
+  // utilisation de l'appel
+  yield call(delay, 1000);
+  yield put({ type: "INCREMENT" });
 }
 
 // qui regarde le sagaet crée une nouvelle tache à chaque incrementation
 function* watchIncrementAsync() {
-  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+  yield takeEvery("INCREMENT_ASYNC", incrementAsync);
 }
 
 // l'export de toutes fonctions avec rootSaga qui reprends les function à exporter
 //un tableau de résultats avec les sagas exporter
 //les 2 générateurs sont executé en parallele et appeler par sagaMiddleware.run a la racine "main.js"
 export default function* rootSaga() {
-  yield all([
-    helloSaga(),
-    watchIncrementAsync()
-  ])
+  yield all([helloSaga(), watchIncrementAsync()]);
 }
